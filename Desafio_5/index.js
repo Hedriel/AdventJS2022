1471b5783d11cc99c9832dff1c074b45fd420191
Hernan Gonzalez <hernan.gonzalez.magliano@gmail.com>
2022-12-22 01:15:30 -0300

    So far the best solution I came up with. Still cant find the way to optimize the trips

--- Desafio_5/index.js
+++ Desafio_5/index.js
@@ -1,38 +1,26 @@
-const giftsCities = [12, 3, 11, 5, 7, 50] //38
+const giftsCities = [12, 3, 11, 5, 7]
 const maxGifts = 20
 const maxCities = 3
 
 function getMaxGifts(giftsCities, maxGifts, maxCities) {
-  let sortedGiftsCities = giftsCities.sort((a, b) => b - a).filter(a => a <= maxGifts)
+  let citiesToDeliver = giftsCities
+  .filter( city => city <= maxGifts)
+  .sort((a, b) => {return b-a})
   let deliveredGifts = 0
-  
-  const giftsCity = sortedGiftsCities[0];
-  const sortedGiftCitiesSlice = sortedGiftsCities.slice(1);
-  let currentDeliveredGifts = giftsCity;
-  
-  for (let i = 0; i < sortedGiftCitiesSlice.length; i++) {
-    if (sortedGiftsCities.length == 0) return 0
-    if (sortedGiftsCities[0] == maxGifts || maxCities == 1 ) return sortedGiftsCities[0]
 
-    const giftsToDeliver = sortedGiftCitiesSlice[i];    
-    currentDeliveredGiftsWannaBe = currentDeliveredGifts + giftsToDeliver
+  console.log(citiesToDeliver)
 
-    if (currentDeliveredGiftsWannaBe <= maxGifts) {
-      deliveredGifts = currentDeliveredGiftsWannaBe
+  citiesToDeliver.forEach(city => {
+    if (city <= maxGifts &&  maxGifts >= city+deliveredGifts) {
+      deliveredGifts = deliveredGifts + city
     }
-   
-  }
+  });
   return deliveredGifts
 }
 
-  /*   let giftsToGift = giftsCities.reduce((a,b) => a + b)
-    let giftedGift = 0
-  
-      while (giftedGift <= (giftsToGift<maxGifts?giftsToGift:maxGifts) && giftedGift <= maxGiftPerCity) {
-        console.log(giftedGift)
-        giftedGift++
-      }
-      return giftedGift -1 */
 
-
-console.log(getMaxGifts([12, 3, 11, 5, 7], 20, 3)) // 20
+/* while (giftedGift <= (giftsToGift<maxGifts?giftsToGift:maxGifts) && giftedGift <= maxGiftPerCity) {
+  console.log(giftedGift)
+  giftedGift++
+} */
+console.log(getMaxGifts([12, 3, 11, 5, 7], 20, 3))
